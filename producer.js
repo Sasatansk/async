01d2b3637de85df7710473dab53704020a05959c
Sasatansk <161496323+Sasatansk@users.noreply.github.com>
2025-03-27 00:14:25 +0000

    add.consumidor

---
+++ producer.js
@@ -0,0 +1,26 @@
+const amqp = require("amqplib");
+
+async function produce () {
+    try {
+        const connection = await amqp.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        const queue = 'mensagens';
+        await channel.assertQueue(queue,{durabke:false});
+
+        let messageCount = 1;
+
+        setInterval(() => {
+
+            const message = `Messagem ${messageCount}`;
+            channel.sendToQueue(queue, Buffer.from(message));
+            console.log(`Enviada : ${mensagem}`);
+            messageCount++;
+            }, 2000);
+
+   }catch (error){
+        console.error("Erro:", error);
+    
+   }
+    
+}
+produce ();
